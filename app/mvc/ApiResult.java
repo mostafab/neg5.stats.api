90fa757a16e11a5e4e207090347c3645153bb37c
mostafab <mostafa0104@gmail.com>
2017-12-02 22:46:44 -0800

    Initial commit

---
+++ app/mvc/ApiResult.java
@@ -0,0 +1,37 @@
+package mvc;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import play.libs.Json;
+import play.mvc.Result;
+
+import static play.mvc.Results.ok;
+
+/**
+ *
+ * @param <T>
+ */
+public class ApiResult<T> extends Result {
+
+    private T payload;
+    private int statusCode;
+
+    public ApiResult() {
+        super(-1);
+        this.statusCode = statusCode;
+    }
+
+    public Result success(T payload) {
+        this.statusCode = 200;
+        this.payload = payload;
+        JsonNode json = Json.toJson(this);
+        return ok(json);
+    }
+
+    public T getPayload() {
+        return payload;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
